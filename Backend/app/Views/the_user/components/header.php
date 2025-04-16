fa6efd0e01dfc29496e4553ea97dfb1bf3cc0517
unknown <20mm313@esisalama.org>
2025-04-16 20:23:54 +0200

    navbar...

--- Backend/app/Views/the_user/components/header.php
+++ Backend/app/Views/the_user/components/header.php
@@ -5,10 +5,10 @@ $current_page = basename($_SERVER['REQUEST_URI']);
 <header id="header" class="fixed-top d-flex align-items-center header-transparent entete">
   <div class="container-fluid">
     <div class="">
-      <div class="align-items-center justify-content-between entete">
-        <div class="justify-content-center align-items-center">
+      <div class="h-100 w-100 d-flex flex-lg-column align-items-center justify-content-between">
+        <div class="w-100 justify-content-lg-center align-items-center">
           <h1 class="logo">
-            <a href="#" class="">
+            <a href="#" class="d-flex justify-content-start justify-content-lg-center align-items-center">
               <img src="assets/img/favicon_io/logo-asunicaco.png" width="53" heigth="53" alt="Funda">
               <span>ASUNICACO</span>
             </a>
@@ -16,7 +16,7 @@ $current_page = basename($_SERVER['REQUEST_URI']);
         </div>
 
         <nav id="navbar" class="navbar">
-          <ul class="justify-content-center align-items-center">
+          <ul class="justify-content-lg-center align-items-lg-center">
             <li><a class="nav-link <?= $current_page == '' ? 'active' : '' ?>" href="/Asunicaco/public/">Accueil</a></li>
             <li><a class="nav-link <?= $current_page == 'actualites' ? 'active' : '' ?>" href="/Asunicaco/public/actualites">Actualités</a></li>
             <li><a class="nav-link <?= $current_page == 'provinces' ? 'active' : '' ?>" href="/Asunicaco/public/provinces">Provinces</a></li>
@@ -24,7 +24,7 @@ $current_page = basename($_SERVER['REQUEST_URI']);
             <li><a class="nav-link <?= $current_page == 'contact' ? 'active' : '' ?>" href="/Asunicaco/public/contact">Contact</a></li>
           </ul>
 
-          <div class="d-block d-lg-none">
+          <div class="d-block d-lg-none justify-content-end">
             <i class="bi bi-list mobile-nav-toggle"></i>
           </div>
         </nav>
