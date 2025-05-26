a1ea432756e7fce2038dc7815037b05c2d9d038f
unknown <20mm313@esisalama.org>
2025-05-26 23:16:43 +0200

    apropos page

--- Backend/app/Views/the_user/pages/apropos.php
+++ Backend/app/Views/the_user/pages/apropos.php
@@ -1,10 +1,9 @@
 <section class="hero-section pt-5 position-relative d-flex align-items-center justify-content-center text-white" style="background-image: url('assets/img/apropos/1.jpg'); background-size: cover; background-position: center; height: 80vh;">
-  <div 
-    class="position-absolute top-0 start-0 w-100 h-100" 
-  </div>
+  <div
+    class="position-absolute top-0 start-0 w-100 h-100"
+    </div>
 </section>
 
-<!-- Fil d'Ariane -->
 <div class="container my-3">
   <!-- <small><a href="#">Accueil</a> / À propos</small> -->
   <small><a href="index.php" style="color: #2952A1;">Accueil</a> / <span style="color: black;">À propos</span></small>
@@ -30,16 +29,18 @@
 
   <div class="row mt-4">
     <div class="col-md-3 fw-bold">Date de création et Siège</div>
-    <div class="col-md-9">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore itaque perspiciatis iste nesciunt? Sit corrupti impedit deleniti aut dolores temporibus nemo voluptas odio neque vero?</div>
+    <div class="col-md-9">
+      <p>L'ASUNICACO a été créée le <strong>9 juin 2010</strong>. Son siège social est situé au <strong>Centre interdiocésain, Avenue Virunga n°59, Kinshasa-Gombe</strong> (RD Congo). Ce siège est stable et ne peut être déplacé que pour des raisons graves.</p>
+    </div>
   </div>
 
   <div class="row mt-4">
     <div class="col-md-3 fw-bold">Objectifs</div>
     <div class="col-md-9">
       <ul>
-        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore itaque perspiciatis iste nesciunt?</li>
-        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore itaque perspiciatis iste nesciunt?</li>
-        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore itaque perspiciatis iste nesciunt?</li>
+        <li>Soutenir l'Église dans son œuvre d'évangélisation et promouvoir le développement, la justice et la paix.</li>
+        <li>Renforcer l'identité catholique des institutions membres et favoriser leur coopération académique et scientifique.</li>
+        <li>Promouvoir une éducation de qualité par l'enseignement, la recherche et des projets communs au service de la société congolaise.</li>
       </ul>
     </div>
   </div>
@@ -48,18 +49,19 @@
     <div class="col-md-3 fw-bold">Conditions d'adhésion</div>
     <div class="col-md-9">
       <ol>
-        <li>Être agréé par ...</li>
-        <li>Être fonctionnel en RD. Congo</li>
-        <li>Introduire sa demande ...</li>
-        <li>Dolore itaque perspiciatis iste nesciunt</li>
-        <li>Sit corrupti impedit delen</li>
-        <li>Et consectetur adipisicing elit</li>
+        <li>Être agréé par l'autorité ecclésiastique compétente (diocèse ou congrégation).</li>
+        <li>Être un établissement d'enseignement supérieur catholique fonctionnel en RD Congo.</li>
+        <li>Introduire une demande écrite au président, recommandée par deux institutions membres.</li>
+        <li>Participer à au moins une Assemblée générale en tant qu'observateur avant l'admission.</li>
+        <li>Obtenir l'approbation par un vote à la majorité des 2/3 des membres lors de l'Assemblée générale.</li>
       </ol>
     </div>
   </div>
 
   <div class="row mt-4">
     <div class="col-md-3 fw-bold">Bureau du comité exécutif</div>
-    <div class="col-md-9">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore itaque perspiciatis iste nesciunt? Sit corrupti impedit deleniti aut dolores temporibus nemo voluptas odio neque vero!</div>
+    <div class="col-md-9">
+      <p>Le comité exécutif est composé du <strong>Président</strong> (élu pour 3 ans renouvelable), du <strong>Vice-président</strong>, du <strong>Secrétaire</strong>, du <strong>Trésorier</strong>, de <strong>six membres du comité scientifique</strong> et d'un <strong>représentant des fondateurs</strong>. Il assure la gestion quotidienne et l'exécution des décisions de l'Assemblée générale.</p>
+    </div>
   </div>
-</div>
+</div>``
