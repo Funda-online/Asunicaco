93ea3b9a0277d88d86967edebac80ab587d1bac0
unknown <20mm313@esisalama.org>
2025-06-12 00:13:14 +0200

    actualites 1

--- Backend/app/Views/the_admin/pages/adminActualites/actualites.php
+++ Backend/app/Views/the_admin/pages/adminActualites/actualites.php
@@ -1,1 +1,59 @@
-<section>Actualités</section>
+<div class="container-fluid">
+    <div class="d-flex align-items-center justify-content-between mb-4">
+        <h1 class="h4 mb-0 text-gray-800">Gestion actualités</h1>
+
+        <div>
+            <button class="btn btn-md text-white" style="background-color: #2952A1; font-size: 14px;">Nouvelle actualité</button>
+        </div>
+    </div>
+
+    <!-- actus -->
+    <div class="card-body p-0">
+        <div>
+            <div class="row">
+                <!-- Start col -->
+                <div class="col-md-8">
+                    <!--begin::Row-->
+                    <div class="row g-4 mb-4">
+                        <div class="col-md-6">
+                            <!-- USERS LIST -->
+                            <div class="card">
+                                <div class="card-header">
+                                    <h3 class="card-title">Représentation brillante de l'UCC à Lille en France</h3>
+                                    <div class="card-tools">
+                                        <button type="button" class="btn btn-tool" data-lte-toggle="card-collapse">
+                                            <i data-lte-icon="expand" class="bi bi-plus-lg"></i>
+                                            <i data-lte-icon="collapse" class="bi bi-dash-lg"></i>
+                                        </button>
+                                        <button type="button" class="btn btn-tool" data-lte-toggle="card-remove">
+                                            <i class="bi bi-x-lg"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                                <!-- /.card-header -->
+                                <div class="card-body p-0">
+                                    <div class="row m-1 image-dashboard">
+                                        <img src="../assets/img/article.jpg" alt="" class="">
+                                        <p>
+                                            Pascaline KABEMBA a présenté la palabre africaine comme modèle de communication à la
+                                            Journée des jeunes chercheur·e·s en SIC. Elle critique les paradigmes occidentaux et
+                                            plaide pour une valorisation des pratiques africaines face aux crises du continent.
+                                        </p>
+                                    </div>
+                                </div>
+                                <!-- /.card-body -->
+                                <div class="card-footer text-center">
+                                    <a href="javascript:"
+                                        class="link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Editer</a>
+                                </div>
+                                <!-- /.card-footer -->
+                            </div>
+                            <!-- /.card -->
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
