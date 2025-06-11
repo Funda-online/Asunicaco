58fb887abf9c780f5495da8fd64ba8cf5ce2c4ea
unknown <20mm313@esisalama.org>
2025-06-11 17:00:39 +0200

    dashboard 1

--- Backend/app/Views/the_admin/pages/dashboard.php
+++ Backend/app/Views/the_admin/pages/dashboard.php
@@ -1,9 +1,19 @@
 <div class="container-fluid">
 
     <!-- Page Heading -->
-    <div class="d-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-    </div>
+    <!-- <div class="d-flex align-items-center justify-content-between mb-4"> -->
+        <!-- <h1 class="h3 mb-0 text-gray-800">Dashboard</h1> -->
+
+        <div class="d-flex align-items-center justify-content-between mb-4">
+              <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+              <div>
+                <ol class="breadcrumb float-sm-end">
+                  <li class="breadcrumb-item"><a href="#">Home</a></li>
+                  <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+                </ol>
+              </div>
+            </div>
+    <!-- </div> -->
 
     <!-- Content Row -->
     <div class="row">
